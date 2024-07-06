aa3af03958ed458f433a1c43a1f173608ec06a0e
Ignacio <ignacio.f.zuniga@gmail.com>
2024-07-06 01:56:18 -0400

    [UPDATE] info

--- frontend/src/components/NameTitle/NameTitle.js
+++ frontend/src/components/NameTitle/NameTitle.js
@@ -1,9 +1,11 @@
 import './NameTitle.css';
 import { useTranslation } from 'react-i18next';
+// import { Row, Col } from "react-bootstrap";
 
 const NameTitle = () => {
     const { t } = useTranslation();
     return(
+        <>
             <div className="blue-background title-container white-text">
                 <div className="full-name">
                     José Ignacio Figueroa Zúñiga
@@ -12,6 +14,21 @@ const NameTitle = () => {
                     {t("title")}
                 </div>
             </div>
+            {/* <Row>
+                <Col>
+                    <a href="tel:+56951386005">+569 5138 6005</a>
+                </Col>
+                <Col>
+                    <a href="mailto:ignacio.f.zuniga@gmail.com">ignacio.f.zuniga@gmail.com</a>
+                </Col>
+                <Col>
+                    Avenida San Joaquín 0710, depto 703, Rancagua, Chile.
+                </Col>
+                <Col>
+                    <a href="https://www.linkedin.com/in/igna-fig/" target="_blank">/igna-fig/</a>
+                </Col>
+            </Row> */}
+        </>
     );
 };
 
