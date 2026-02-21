d5dddaab446b21925c436ee09943427dfb2e80d8
Ignacio Figueroa <jfigueroaz@ripley.com>
2026-02-21 16:12:06 -0300

    [UPDATE] added PDF download

--- frontend/src/components/NameTitle/NameTitle.js
+++ frontend/src/components/NameTitle/NameTitle.js
@@ -1,7 +1,8 @@
 import "./NameTitle.css";
 import { useTranslation } from "react-i18next";
 import { Row, Col } from "react-bootstrap";
-// import { Row, Col } from "react-bootstrap";
+import { FaDownload } from "react-icons/fa";
+import Resume from "../../assets/resume.pdf";
 
 const NameTitle = () => {
   const { t } = useTranslation();
@@ -12,21 +13,19 @@ const NameTitle = () => {
           <div className="full-name">José Ignacio Figueroa Zúñiga</div>
           <div className="subtitle white-text">{t("title")}</div>
         </Col>
+        <Col
+          className="d-flex justify-content-end align-items-center"
+          style={{ color: "#fff !important" }}
+        >
+          <a
+            href={Resume}
+            style={{ color: "#fff !important" }}
+            download="Jose_Figueroa_Resume.pdf"
+          >
+            <FaDownload size={30} />
+          </a>
+        </Col>
       </Row>
-      {/* <Row>
-                <Col>
-                    <a href="tel:+56951386005">+569 5138 6005</a>
-                </Col>
-                <Col>
-                    <a href="mailto:ignacio.f.zuniga@gmail.com">ignacio.f.zuniga@gmail.com</a>
-                </Col>
-                <Col>
-                    Avenida San Joaquín 0710, depto 703, Rancagua, Chile.
-                </Col>
-                <Col>
-                    <a href="https://www.linkedin.com/in/igna-fig/" target="_blank">/igna-fig/</a>
-                </Col>
-            </Row> */}
     </>
   );
 };
